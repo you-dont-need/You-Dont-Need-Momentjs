9be9304621f51016bd235902f103f60459881fd7
Robert Chang <cht8687@gmail.com>
2018-09-10 00:05:27 +1000

    Prettier code

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,6 +1,6 @@
-const moment = require('moment');
-const date = require('date-fns');
-const fr = require('date-fns/locale/fr');
+const moment = require("moment");
+const date = require("date-fns");
+const fr = require("date-fns/locale/fr");
 
 const time = 1536484369695;
 
@@ -17,10 +17,10 @@ describe("Parse", () => {
   });
   it("String + Format + locale", () => {
     const m = moment("2012 mars", "YYYY MMM", "fr");
-    const d = date.parse('2012 mars', "yyyy MMMM", new Date(), { locale: fr });
+    const d = date.parse("2012 mars", "yyyy MMMM", new Date(), { locale: fr });
     expect(m.valueOf()).toBe(d.getTime());
-  })
-})
+  });
+});
 
 describe("Get + Set", () => {
   it("get Second", () => {
@@ -29,7 +29,9 @@ describe("Get + Set", () => {
     expect(m).toBe(d);
   });
   it("set Second", () => {
-    const m = moment(time).seconds(30).valueOf();
+    const m = moment(time)
+      .seconds(30)
+      .valueOf();
     const d = new Date(time).setSeconds(30);
     expect(m).toBe(d);
   });
@@ -40,7 +42,9 @@ describe("Get + Set", () => {
     expect(m).toBe(d);
   });
   it("set Hour", () => {
-    const m = moment(time).hour(13).valueOf();
+    const m = moment(time)
+      .hour(13)
+      .valueOf();
     const d = new Date(time).setHours(13);
     expect(m).toBe(d);
   });
@@ -52,7 +56,9 @@ describe("Get + Set", () => {
   });
 
   it("set Date of Month", () => {
-    const m = moment(time).date(4).valueOf();
+    const m = moment(time)
+      .date(4)
+      .valueOf();
     const d = new Date(time).setDate(4);
     expect(m).toBe(d);
   });
@@ -64,8 +70,10 @@ describe("Get + Set", () => {
   });
 
   it("set Day of Week", () => {
-    const m = moment(time).day(-14).valueOf();
-    const d = new Date(time).setDate(new Date(time).getDate() - 14)
+    const m = moment(time)
+      .day(-14)
+      .valueOf();
+    const d = new Date(time).setDate(new Date(time).getDate() - 14);
     expect(m).toBe(d);
   });
 
@@ -76,7 +84,9 @@ describe("Get + Set", () => {
   });
 
   it("set Day of Year", () => {
-    const m = moment(time).dayOfYear(256).valueOf();
+    const m = moment(time)
+      .dayOfYear(256)
+      .valueOf();
     const d = date.setDayOfYear(new Date(time), 256).getTime();
     expect(m).toBe(d);
   });
@@ -88,7 +98,9 @@ describe("Get + Set", () => {
   });
 
   it("set Week of Year", () => {
-    const m = moment(time).week(24).valueOf();
+    const m = moment(time)
+      .week(24)
+      .valueOf();
     const d = date.setWeek(new Date(time), 24).getTime();
     expect(m).toBe(d);
   });
@@ -104,7 +116,7 @@ describe("Get + Set", () => {
       new Date(2017, 4, 13),
       new Date(2018, 2, 12),
       new Date(2016, 0, 10),
-      new Date(2016, 0, 9),
+      new Date(2016, 0, 9)
     ];
     const m = moment.max(array.map(a => moment(a)));
     const d = date.max(array);
@@ -117,40 +129,40 @@ describe("Get + Set", () => {
       new Date(2017, 4, 13),
       new Date(2018, 2, 12),
       new Date(2016, 0, 10),
-      new Date(2016, 0, 9),
+      new Date(2016, 0, 9)
     ];
     const m = moment.min(array.map(a => moment(a)));
     const d = date.min(array);
     expect(m.valueOf()).toBe(d.getTime());
     expect(d).toEqual(new Date(2016, 0, 9));
   });
-})
+});
 
 describe("Manipulate", () => {
   it("Add", () => {
-    const m = moment(time).add(7, 'days');
+    const m = moment(time).add(7, "days");
     const d = date.addDays(new Date(time), 7);
     expect(m.valueOf()).toBe(d.getTime());
   });
 
   it("Subtract", () => {
-    const m = moment(time).subtract(7, 'days');
+    const m = moment(time).subtract(7, "days");
     const d = date.subDays(new Date(time), 7);
     expect(m.valueOf()).toBe(d.getTime());
   });
 
   it("Start of Time", () => {
-    const m = moment(time).startOf('month');
+    const m = moment(time).startOf("month");
     const d = date.startOfMonth(new Date(time));
     expect(m.valueOf()).toBe(d.getTime());
   });
 
   it("End of Time", () => {
-    const m = moment(time).endOf('day');
+    const m = moment(time).endOf("day");
     const d = date.endOfDay(new Date(time));
     expect(m.valueOf()).toBe(d.getTime());
   });
-})
+});
 
 describe("Display", () => {
   it("Format", () => {
@@ -165,23 +177,31 @@ describe("Display", () => {
 
   it("Time from now", () => {
     const m = moment(time).fromNow();
-    const d = date.formatDistance(new Date(time), new Date(), { addSuffix: true });
+    const d = date.formatDistance(new Date(time), new Date(), {
+      addSuffix: true
+    });
     expect(d).toContain(m);
   });
 
   it("Time from X", () => {
-    const m = moment([ 2007, 0, 27 ]).to(moment([ 2007, 0, 29 ]));
+    const m = moment([2007, 0, 27]).to(moment([2007, 0, 29]));
     const d = date.formatDistance(new Date(2007, 0, 27), new Date(2007, 0, 29));
     expect(m).toContain(d);
   });
 
   it("Difference", () => {
-    const m = moment([ 2007, 0, 27 ]).diff(moment([ 2007, 0, 29 ]));
-    const d = date.differenceInMilliseconds(new Date(2007, 0, 27), new Date(2007, 0, 29));
+    const m = moment([2007, 0, 27]).diff(moment([2007, 0, 29]));
+    const d = date.differenceInMilliseconds(
+      new Date(2007, 0, 27),
+      new Date(2007, 0, 29)
+    );
     expect(m).toBe(d);
 
-    const m2 = moment([ 2007, 0, 27 ]).diff(moment([ 2007, 0, 29 ]), 'days');
-    const d2 = date.differenceInDays(new Date(2007, 0, 27), new Date(2007, 0, 29));
+    const m2 = moment([2007, 0, 27]).diff(moment([2007, 0, 29]), "days");
+    const d2 = date.differenceInDays(
+      new Date(2007, 0, 27),
+      new Date(2007, 0, 29)
+    );
     expect(m2).toBe(d2);
   });
 
@@ -189,43 +209,48 @@ describe("Display", () => {
     const m = moment("2012-02", "YYYY-MM").daysInMonth();
     const d = date.getDaysInMonth(new Date(2012, 1));
     expect(m).toBe(d);
-  })
+  });
 });
 
 describe("Query", () => {
   it("Is Before", () => {
-    const m = moment('2010-10-20').isBefore('2010-10-21');
+    const m = moment("2010-10-20").isBefore("2010-10-21");
     const d = date.isBefore(new Date(2010, 9, 20), new Date(2010, 9, 21));
     expect(m).toBeTruthy();
     expect(d).toBeTruthy();
   });
 
   it("Is Same", () => {
-    expect(moment('2010-10-20').isSame('2010-10-21')).toBeFalsy();
-    expect(date.isSameDay(new Date(2010, 9, 20), new Date(2010, 9, 21))).toBeFalsy();
+    expect(moment("2010-10-20").isSame("2010-10-21")).toBeFalsy();
+    expect(
+      date.isSameDay(new Date(2010, 9, 20), new Date(2010, 9, 21))
+    ).toBeFalsy();
 
-    expect(moment('2010-10-20').isSame('2010-10-21', 'month')).toBeTruthy();
-    expect(date.isSameMonth(new Date(2010, 9, 20), new Date(2010, 9, 21))).toBeTruthy();
+    expect(moment("2010-10-20").isSame("2010-10-21", "month")).toBeTruthy();
+    expect(
+      date.isSameMonth(new Date(2010, 9, 20), new Date(2010, 9, 21))
+    ).toBeTruthy();
   });
 
   it("Is After", () => {
-    const m = moment('2010-10-20').isAfter('2010-10-19');
+    const m = moment("2010-10-20").isAfter("2010-10-19");
     const d = date.isAfter(new Date(2010, 9, 20), new Date(2010, 9, 19));
     expect(m).toBeTruthy();
     expect(d).toBeTruthy();
   });
 
   it("Is Between", () => {
-    const m = moment('2010-10-20').isBetween('2010-10-19', '2010-10-25');
-    const d = date.isWithinInterval(new Date(2010, 9, 20),
-      { start: new Date(2010, 9, 19), end: new Date(2010, 9, 25) }
-    )
+    const m = moment("2010-10-20").isBetween("2010-10-19", "2010-10-25");
+    const d = date.isWithinInterval(new Date(2010, 9, 20), {
+      start: new Date(2010, 9, 19),
+      end: new Date(2010, 9, 25)
+    });
     expect(m).toBeTruthy();
     expect(d).toBeTruthy();
   });
 
   it("Is Leap Year", () => {
-    expect(moment([ 2000 ]).isLeapYear()).toBeTruthy();
+    expect(moment([2000]).isLeapYear()).toBeTruthy();
     expect(date.isLeapYear(new Date(2000, 0, 1))).toBeTruthy();
     expect(date.isLeapYear(new Date(2001, 0, 1))).toBeFalsy();
   });
@@ -234,4 +259,4 @@ describe("Query", () => {
     expect(moment.isDate(new Date())).toBeTruthy();
     expect(date.isDate(new Date())).toBeTruthy();
   });
-})
+});
